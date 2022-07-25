e27b1fae8675979573e433c4ca8e8205fbb7dab9
Gabriel Konkle <gabriel.konkle@gmail.com>
2022-07-25 01:24:37 -0500

    init

---
+++ app/models/team.server.ts
@@ -0,0 +1,50 @@
+import type { User, Team } from "@prisma/client";
+
+import { prisma } from "~/db.server";
+
+export type { Team } from "@prisma/client";
+
+export function getTeam({
+  id,
+}: Pick<Team, "id"> & {
+  userId: User["id"];
+}) {
+  return prisma.team.findFirst({
+    select: { id: true, name: true, description: true },
+    where: { id },
+  });
+}
+
+export function getTeams({ userId }: { userId: User["id"] }) {
+  return prisma.team.findMany({
+    where: { users: { some: { id: userId } } },
+    select: { id: true, name: true },
+    orderBy: { updatedAt: "desc" },
+  });
+}
+
+// export function createNote({
+//   body,
+//   title,
+//   userId,
+// }: Pick<Note, "body" | "title"> & {
+//   userId: User["id"];
+// }) {
+//   return prisma.note.create({
+//     data: {
+//       title,
+//       body,
+//       user: {
+//         connect: {
+//           id: userId,
+//         },
+//       },
+//     },
+//   });
+// }
+
+export function deleteNote({ id }: Pick<Team, "id">) {
+  return prisma.note.delete({
+    where: { id },
+  });
+}
